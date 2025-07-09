2ded6681edbe6f645b1a11c110001324c903d1a9
lukaspitr <lukas.pitr9@gmail.com>
2025-07-09 15:48:37 +0200

    feat: enhance HeroSlider with scroll functionality and layout adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,18 @@ const images = [
 ];
 
 export default function HeroSlider() {
+  const scrollToNextSection = () => {
+    const nextSection = document.querySelector("#gallery");
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    } else {
+      console.error("Next section not found");
+    }
+  };
   return (
     <>
       {/* Hero sekce */}
-      <div className="relative h-[85vh] text-center overflow-hidden">
+      <div className="relative h-[calc(100vh-85px)] text-center overflow-hidden">
         <Swiper
           modules={[Autoplay, EffectFade]}
           effect="fade"
@@ -33,13 +41,13 @@ export default function HeroSlider() {
                 className="h-full w-full bg-cover bg-center"
                 style={{ backgroundImage: `url(${img})` }}
               >
-                <div className="h-full w-full bg-black/30 flex items-center justify-center">
+                <div className="h-full w-full bg-black/30 flex items-end justify-center py-24">
                   <div className="z-10 px-4">
                     <h1 className="text-4xl md:text-5xl font-bold text-white">
                       Kristýna Pítrová
                     </h1>
                     <p className="mt-2 text-xl italic text-white">
-                      rodinná fotografka 📸
+                      rodinná fotografka
                     </p>
                     <p className="mt-4 text-base text-white/80">
                       Focení srdcem, v uvolněné přátelské atmosféře 💗
@@ -50,10 +58,32 @@ export default function HeroSlider() {
             </SwiperSlide>
           ))}
         </Swiper>
+        {/* Scroll Indicator */}
+        <div
+          className="absolute bottom-2 left-1/2 -translate-x-1/2 z-20 animate-bounce cursor-pointer"
+          onClick={scrollToNextSection}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-white opacity-80 -translate-x-1/2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </div>
       </div>
 
       {/* Galerie & Recenze */}
-      <h2 className="text-3xl font-bold text-center m-6">Galerie</h2>
+      <h2 className="text-3xl font-bold text-center m-6" id="gallery">
+        Galerie
+      </h2>
       <div className="flex flex-col md:flex-row items-center justify-center bg-muted py-12 w-full mx-auto px-4 rounded shadow-md">
         {/* Image on the left */}
         <div className="md:w-1/3 w-full flex justify-center mb-8 md:mb-0 md:mr-8">
